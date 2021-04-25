04923de7d638f36835666c438d39a68de58faacf
AnthonyRodriguez93 <52090741+AnthonyRodriguez93@users.noreply.github.com>
2021-04-25 11:21:56 -0500

    update

--- script.js
+++ script.js
@@ -4,25 +4,49 @@ const amountInput = document.getElementById("amount");
 const btn = document.querySelector("button");
 
 const createRowElement = () => {
+    document.getElementsByClassName("infoRow")[0].remove();
     const row = document.createElement("tr");
     const tbody = document.getElementsByTagName("tbody")[0];
     tbody.appendChild(row);
     let nameElement = document.createElement("td");
     let dateElement = document.createElement("td");
     let amountElement = document.createElement("td");
+    let delbtn = document.createElement("button");
     nameElement.appendChild(document.createTextNode(nameInput.value));
     dateElement.appendChild(document.createTextNode(dateInput.value));
     amountElement.appendChild(document.createTextNode(amountInput.value));
+    delbtn.appendChild(document.createTextNode("X"));
     row.appendChild(nameElement);
     row.appendChild(dateElement);
     row.appendChild(amountElement);
+    row.appendChild(delbtn);
     nameInput.value = "";
     dateInput.value = "";
     amountInput.value = "";
+    addListToBtns(delbtn);
+
 }
 const addListAfterClick = () => {
     if (nameInput.value.length > 0 && amountInput.value.length > 0){
         createRowElement();
+    }
+}
+
+const addListToBtns = (k) => {
+    k.addEventListener("click", GetDeleted);
+}
+
+const GetDeleted = () => {
+    let del = document.querySelectorAll("tr");
+    let n = del.length;
+    let makeHandler = function(num){
+        return function(){
+            this.closest("tr").remove();
+        };
+    };
+
+    for (let i = 0; i < n; i++) {
+        del[i].onclick = makeHandler();
     }
 }
 
